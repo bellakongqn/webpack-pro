0b2a3f5f07a8589c4191fc14e12824942516014e
qingnan.kong <bellakongqn@gmail.com>
2019-09-02 21:20:41 +0800

    react es6 preset-env preset-react babel

---
+++ src/searchBar.js
@@ -0,0 +1,17 @@
+// document.write('Hello Search')
+
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class SearchBar extends React.PureComponent{
+    render(){
+        return(
+           'ggggg'
+        )
+    }
+}
+
+ReactDom.render(
+    <SearchBar />,
+    document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,19 @@
 const path = require('path')
 
 module.exports = {
-    entry:'./src/index.js',
+    entry:{
+        index:'./src/index.js',
+        search:'./src/searchBar.js'
+    },
     output:{
         path:path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
+    },
+    module:{
+        rules:[
+            // 解析Es6 React
+            {test:/.js$/ , use:'babel-loader'}
+        ]
     },
     mode:'production'
 
