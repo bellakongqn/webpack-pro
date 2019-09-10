4509ae1b6d5785881db41a8db8591023e7cbc188
qingnan.kong <bellakongqn@gmail.com>
2019-09-10 11:49:31 +0800

    webpack data initial

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const template = fs.readFileSync(path.join(__dirname, '../dist/search.html'), 'utf-8');
+const data = require('./data.json')
 
 
 const server = (port) =>{
@@ -16,8 +17,7 @@ const server = (port) =>{
     app.use(express.static(path.join(__dirname, '../dist')))
     // 资源权限设置
     app.get('/search' , (req,res) =>{
-        // const html = renderMarkUp(renderToString(SSR))
-        const html = template.replace('<!-- html-placeholder -->', renderToString(SSR));
+        const html = renderMarkUp(renderToString(SSR))
         res.status(200).send(html)
     })
 
@@ -30,5 +30,7 @@ const server = (port) =>{
 server(process.env.PORT||3000)
 
 const renderMarkUp = (str) =>{
+    const dataStr = JSON.stringify(data)
     return template.replace('<!-- html-placeholder -->',str)
+                   .replace('<!-- initial-data-placeholder -->',`<script>window.__initial_data=${dataStr}</script>`)
 }
